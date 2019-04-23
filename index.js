926eab524d7086493af73187b03f4cacd4932937
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-23 02:30:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // write your code below!
+let happyHolidays = () => "Happy holidays!";
+let happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
+let happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`
+let holidayCountdown = (holiday, days) => `It's ${days} days until ${hoiday}!`
