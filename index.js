09fd87a5adc8f16007e04753ef7466510c4dfca1
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-23 02:36:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ let happyHolidays = () => "Happy holidays!";
 let happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
 let happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
 let holidayCountdown = (holiday, days) => `It's ${days} days until ${hoiday}!`;
-
-module.export(happyHolidays, happyHolidaysTo, happyHolidayTo, holidayCountdown);
